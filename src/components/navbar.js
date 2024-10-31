414ce26c6f3d921a77ac6e48bfff52706eff42c2
FaithAchieng <faithachienglexus@gmail.com>
2024-10-31 14:43:29 +0300

    initial commit

---
+++ src/components/navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "../sass/navbar.scss"
+
+function navbar() {
+  return (
+    <div className='container'>
+        <h1>navbar</h1>
+      
+    </div>
+  )
+}
+
+export default navbar
